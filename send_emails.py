bc9f0e474ddd55b57cc641b83d5e479d1e436483
classjek <jekoniak5741@ucla.edu>
2023-01-03 09:58:06 -0800

    Improve subject and body
    Create subjects and bodys that are specific to a given recipient

--- send_emails.py
+++ send_emails.py
@@ -1,5 +1,6 @@
 import os, ssl
 import smtplib
+from email.header import Header
 
 email_sender= os.environ.get('MY_EMAIL')
 email_password= os.environ.get('EMAIL_PASSWORD')
@@ -8,8 +9,12 @@ email_recipient = os.environ.get('EMAIL_RECEIVER')
 smtp_port = 587 
 smtp_server = "smtp.gmail.com"
 
+recipient = "Gracie Abrams"
 # content of message
-message = "Please help"
+subject = f"Subject: {recipient} | UCLA Celebrity Judge Opportunity\n"
+body = f"Hello {recipient}, \n\n On behalf of the UCLA Student Alumni Association..."
+message = subject + body
+
 
 simple_email_context = ssl.create_default_context()
 
