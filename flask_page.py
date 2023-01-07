42ef75f9551f04771ef6409662c7d5eb09e8f859
classjek <jekoniak5741@ucla.edu>
2023-01-06 22:06:10 -0800

    Input functionality for html testing display
    Use redirect and url_for to render an html representing user input from
    a form. This is the first step in creating a display where the user can
    input test information and look at the output

--- flask_page.py
+++ flask_page.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -8,7 +8,15 @@ def home():
 
 @app.route("/input/", methods=["POST", "GET"])
 def login():
-    return render_template("input.html")
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("input.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run()
