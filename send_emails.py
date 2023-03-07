687a701917e2a07b248c8b519e8652195c11c994
classjek <jekoniak5741@ucla.edu>
2023-03-06 20:08:54 -0800

    email testing to spring sing coordinators

--- send_emails.py
+++ send_emails.py
@@ -3,12 +3,13 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import jinja2
+import pandas as pd
 
 email_sender= os.environ.get('MY_EMAIL')
 email_password= os.environ.get('EMAIL_PASSWORD')
 #email_recipient = os.environ.get('EMAIL_RECEIVER')
-email_recipient = 'cbradley2002@g.ucla.edu'
-
+#email_recipient = 'nikkiaviv@g.ucla.edu'
+email_recipient = 'jekoniak13@gmail.com'
 
 smtp_port = 587 
 smtp_server = "smtp.gmail.com"
@@ -23,8 +24,9 @@ template_loader = jinja2.FileSystemLoader(searchpath="./templates/")
 template_env = jinja2.Environment(loader=template_loader)
 template = template_env.get_template("invitation.html")
 
+agent = "Jaker Baker"
 inputs = {
-    "agent": "agent",
+    "agent": agent,
     "celeb": "Gracie Abram",
     "celeb_informal": "Gracie",
     "pronoun": "she"
@@ -64,6 +66,47 @@ finally:
     TIE_server.quit()
 """
 
+df = pd.read_csv('outreach.csv')
+for index, row in df.iterrows():
+    manager = row['Manager']
+    celeb = row['Celebrity']
+    email = row['Email']
+    #print(row['Manager'], row['Celebrity'])
+
+    email_recipient = email
+    subject = f"Subject: {celeb} | UCLA Celebrity Judge Opportunity\n"
+
+    # load email 
+    inputs = {
+    "agent": manager,
+    "celeb": celeb,
+    "celeb_informal": celeb,
+    "pronoun": "she"
+    }
+
+    message = template.render(inputs)
+
+    msg = MIMEText(message, "html")
+    msg=MIMEMultipart()
+    msg.attach(MIMEText(message, 'html'))
+    msg["Subject"] = subject
+    msg["From"] = email_sender
+    msg["To"] = email_recipient
+
+
+    simple_email_context = ssl.create_default_context()
+
+    print('send email to', email_recipient, 'managers name is', manager)
+
+    # send email
+    
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(email_sender, email_password)
+        server.sendmail(email_sender, email_recipient, msg.as_string())
+    
+
+"""
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
     server.login(email_sender, email_password)
     server.sendmail(email_sender, email_recipient, msg.as_string())
+"""
